9969b257a22914117adc80993a332ec3f155e320
The-MathsMaster <50160441+The-MathsMaster@users.noreply.github.com>
2020-03-16 22:53:12 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 var system = {
-	fake: new Decimal(0),
-	accuracy: new Decimal(0),
-	real: new Decimal(3),
+	fib1: new Decimal(1),
+	fib2: new Decimal(0),
+	it: 0,
+	phi: new Decimal(1),
 	cont: false,
 	calculate: function () {
-		system.accuracy = system.accuracy.add(2);
-		system.fake = system.fake.add(new Decimal(4).div(system.accuracy.mul(system.accuracy.add(1)).mul(system.accuracy.add(2))));
-		system.accuracy = system.accuracy.add(2);
-		system.fake = system.fake.sub(new Decimal(4).div(system.accuracy.mul(system.accuracy.add(1)).mul(system.accuracy.add(2))));
+		system.it += 1;
+		system.fib1 = system.fib1.add(system.fib2);
+		system.fib2 = system.fib2.add(system.fib1);
 	}
 }
-Decimal.set({ precision: 1500 })
 var x;
-function calculatepi() {
+function calculatephi() {
 	if (!system.cont) {
 		x = setInterval(function () {
 			for (var i = 0; i < 2000; i++){
 				system.calculate();
 			}
-			system.real = system.fake.add(3);
-			document.getElementById("pi").innerHTML = system.real.toString();
+			Decimal.set({ precision: system.it*10 })
+			if (system.fib1.gte(system.fib2)) {
+				system.phi = system.fib1.div(system.fib2);
+			} else {
+				system.phi = system.fib2.div(system.fib1);
+			}
+			document.getElementById("phi").innerHTML = system.phi.toString();
 		}, 10)
-		document.getElementById("clickMe").innerHTML = "Click me to stop calculating pi";
+		document.getElementById("clickMe").innerHTML = "Click me or press \"C\" to stop calculating phi";
 		system.cont = true;
 	} else {
 		clearInterval(x);
-		document.getElementById("clickMe").innerHTML = "Click me to start calculating pi";
+		document.getElementById("clickMe").innerHTML = "Click me or press \"C\" to start calculating phi";
 		system.cont = false;
 	}
 }
+document.addEventListener("keydown", function (e) {
+	if (e.which == 67) {
+		calculatephi();
+	}
+});
